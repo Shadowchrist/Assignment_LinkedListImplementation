50871503c8354a7e53b7314dbeaac92f26cd36e8
Shadowchrist <sbhm.prasad@gmail.com>
2020-10-19 00:35:49 +0530

    UC2-UC4: nodes added to top, to bottom and in the middle : committed

--- src/main/java/linkedList/LinkedList.java
+++ src/main/java/linkedList/LinkedList.java
@@ -1,7 +1,5 @@
 package linkedList;
 
-import linkedList.MyNode;
-
 public class LinkedList<K> {
 	
 	public MyNode<K> head;
@@ -12,4 +10,60 @@ public class LinkedList<K> {
 		this.head=head;
 		this.tail=tail;
 	}
-}
+	
+	public MyNode<K> getHead() {
+		return head;
+	}
+
+	public void setHead(MyNode<K> head) {
+		this.head = head;
+	}
+
+	public MyNode<K> getTail() {
+		return tail;
+	}
+
+	public void setTail(MyNode<K> tail) {
+		this.tail = tail;
+	}
+	
+	public void addNode(MyNode<K> newNode) {
+		if (tail == null)
+			setTail(newNode);
+		if (head == null)
+			setHead(newNode);
+		else {
+			MyNode<K> tempNode = this.head;
+			setHead(newNode);
+			newNode.setNext(tempNode);
+		}
+	}
+	
+	public void appendNode(MyNode<K> newNode) {
+		if (head == null)
+			setHead(newNode);
+		if (tail == null)
+			setTail(newNode);		
+		else {
+			this.tail.setNext(newNode);
+			setTail(newNode);			
+		}
+	}
+	
+	public void insertNode(MyNode<K> targetNode, MyNode<K> newNode) {
+		MyNode<K> tempNode=targetNode.getNext();
+		targetNode.setNext(newNode);
+		newNode.setNext(tempNode);
+	}
+
+	public void printLinkedList(MyNode<K> head)
+	{
+		MyNode<K> temp=head;
+		while(temp.getNext()!=null)
+		{
+			System.out.print(temp.getKey()+"->");
+			temp=temp.getNext();
+		}
+		System.out.print(temp.getKey()+"\n");
+	}
+}	
--- src/test/java/linkedList/LinkedListTest.java
+++ src/test/java/linkedList/LinkedListTest.java
@@ -7,13 +7,68 @@ import org.junit.Test;
 public class LinkedListTest {
 
 	@Test
-	public void test() {
-		MyNode<Integer> firstNode=new MyNode<>(56);
-		MyNode<Integer> secondNode=new MyNode<>(30);
-		MyNode<Integer> thirdNode=new MyNode<>(70);
+	public void Adding3NumbersToLinkedList() {
+		MyNode firstNode=new MyNode(56);
+		MyNode secondNode=new MyNode(30);
+		MyNode thirdNode=new MyNode(70);
 		firstNode.setNext(secondNode);
 		secondNode.setNext(thirdNode);
 		boolean result=firstNode.getNext().equals(secondNode)&&secondNode.getNext().equals(thirdNode);
 		assertTrue(result);
 	}
+	
+	@Test
+	public void given3NumberWhenAddedToLinkedShouldBeAddedToTop() {
+		MyNode firstNode = new MyNode(70);
+		MyNode secondNode = new MyNode(30);
+		MyNode thirdNode = new MyNode(56);
+
+		LinkedList<Object> list=new LinkedList<Object>(null,null);
+		
+		list.addNode(firstNode);
+		list.addNode(secondNode);
+		list.addNode(thirdNode);
+		
+		list.printLinkedList(list.getHead());
+		boolean result = list.getHead().equals(thirdNode) && list.getHead().getNext().equals(secondNode)
+				&& list.getTail().equals(firstNode);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void given3NumberWhenAppendedToLinkedShouldBeAddedToBottom() {
+		MyNode firstNode = new MyNode(56);
+		MyNode secondNode = new MyNode(30);
+		MyNode thirdNode = new MyNode(70);
+		
+		LinkedList<Object> list=new LinkedList<Object>(null,null);
+		
+		list.appendNode(firstNode);
+		list.appendNode(secondNode);
+		list.appendNode(thirdNode);
+		
+		list.printLinkedList(list.getHead());
+		boolean result = list.getHead().equals(firstNode) && list.getHead().getNext().equals(secondNode)
+				&& list.getTail().equals(thirdNode);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void given3NumberWhenInseringInBetweenShouldPassLinkedListTest() {
+		MyNode firstNode = new MyNode(56);
+		MyNode secondNode = new MyNode(30);
+		MyNode thirdNode = new MyNode(70);
+
+		LinkedList<Object> list=new LinkedList<Object>(null,null);
+		
+		list.appendNode(firstNode);
+		list.appendNode(thirdNode);
+		list.insertNode(firstNode,secondNode);
+		
+		list.printLinkedList(list.getHead());
+		boolean result = list.getHead().equals(firstNode) && list.getHead().getNext().equals(secondNode)
+				&& list.getTail().equals(thirdNode);
+		assertTrue(result);
+	}
+
 }
